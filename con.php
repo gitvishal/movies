94545b7e28731cfb01f4b6c891eb21a0d01d1816
gitvishal <gitvishalkerkar@gmail.com>
2015-07-30 17:57:16 +0530

    Update con.php

--- con.php
+++ con.php
@@ -4,7 +4,11 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "films";
 
-mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
+//mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
 
-mysql_select_db($dbname) or die(mysql_error());
+//mysql_select_db($dbname) or die(mysql_error());
+$mysqli = new mysqli($dbhost,$dbuser, $dbpass, $dbname);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
 ?>
